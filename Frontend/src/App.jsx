e3602087b4537db9fc3c91ab5c8155071d42fbad
Marine Suhard <marine.suhard@gmail.com>
2024-11-01 22:17:55 +0100

    footer done

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,12 +1,15 @@
 import NavBar from "./components/NavBar.jsx";
+import Footer from "./components/Footer.jsx"
 
 function App() {
   return (
     <div>
       <NavBar />
+      <Footer />
       <main>
         <h1>Welcome to My Website</h1>
       </main>
+     
     </div>
   );
 }
